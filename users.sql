d7947699a929c5c3cd925596047d0d272b4e8378
Ubuntu <ubuntu@ip-172-31-31-148.us-west-1.compute.internal>
2024-05-18 07:57:34 +0000

    added changes to the users.sql to work

--- users.sql
+++ users.sql
@@ -2,8 +2,9 @@
 
 create table users (
     user_id VARCHAR(100) PRIMARY KEY,
-	username VARCHAR (50) UNIQUE NOT NULL,
-    email VARCHAR (355) UNIQUE NOT NULL
+    username VARCHAR (50) UNIQUE NOT NULL,
+    email VARCHAR (355) UNIQUE NOT NULL,
+    best_score INT
 );
 
 
@@ -13,7 +14,7 @@ create table users (
 --     user_id VARCHAR(100) NOT NULL REFERENCES users(user_id),
     
 -- );
-INSERT INTO users (username, email) VALUES ('admin', 'admin@example.com');
+INSERT INTO users (user_id, username, email, best_score) VALUES ('1','narayan', 'narayan.balasubramanian@sjsu.edu', 0);
 
 -- this for seeding user but i need to hash the password
 -- INSERT INTO users (user_id, username, email, password, )
